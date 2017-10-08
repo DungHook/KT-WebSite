6fca9179a898d4a099b25243d113a74439db69a8
DungHook <Vandweller@protonmail.com>
2017-10-08 10:19:04 -0700

    IT WORKS !!!!!!!!!!!!!
    Hover over a name in the web page shows a fixed image
    move mouse away from name and image hides

--- myScript.js
+++ myScript.js
@@ -3,4 +3,6 @@ function ShowFixed() {
     document.getElementById('MDimage').style.display = 'block';
 }
  
- 
+function HideFixed() {
+    document.getElementById('MDimage').style.display = 'none';
+}
