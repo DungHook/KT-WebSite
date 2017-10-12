a6590a2bf57157ecc5489d747fa0f2dbbdfe2edd
DungHook <Vandweller@protonmail.com>
2017-10-12 15:19:37 -0700

    2nd page
    Working on second page, Looks good

--- myScript.js
+++ myScript.js
@@ -1,24 +1,28 @@
 ﻿ 
 function ShowFixed() {
     document.getElementById('MDimage').style.display = 'block';
-}
- 
+} 
 function HideFixed() {
     document.getElementById('MDimage').style.display = 'none';
 }
-
+//
 function ShowSkill() {
     document.getElementById('skillcaps').style.display = 'block';
 }
-
 function HideSkill() {
     document.getElementById('skillcaps').style.display = 'none';
 }
-
+//
 function ShowStat() {
     document.getElementById('statcaps').style.display = 'block';
 }
-
 function HideStat() {
     document.getElementById('statcaps').style.display = 'none';
+}
+//
+function showhouse() {
+    document.getElementById('house').style.display = 'block';
+}
+function hidehouse() {
+    document.getElementById('house').style.display = 'none';
 }
